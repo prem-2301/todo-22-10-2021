832092e6ff50fa6a59d56b7de1302f5c5d9412c8
prem-2301 <aaronstone246@gmail.com>
2021-11-06 17:39:42 +0530

    list

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,57 +1,61 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import app from "./base";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { getAuth, signOut } from "firebase/auth";
-import { AuthContext } from "./Auth";
-
-var roleid;
 
 const auth = getAuth(app);
-const db = getFirestore();
+
+//List
+const list = [];
+
+
 
 const Home = () => {
-    const {currentUser} = useContext(AuthContext);
-    const [roleVar, setRoleVar] = useState(null);
-    const docRef = doc(db, "users", currentUser.uid);
-    const docSnap = getDoc(docRef);
-    docSnap.then(doc =>{
-        roleid = doc.data().role;
-        setRoleVar(roleid);
-        const html =`
-            <div> ${roleid} </div>
-        `;
-        
-        document.querySelector('#Role').innerHTML = html;
-        console.log(doc.data().role);
-        
-    })
 
+    const [tasks, setTasks] = useState(list);
+    const [task, setTask] = useState('');
 
-    if(roleVar === 'owner'){
-        const char = `
-            <div> You are Owner </div>
-        `;
-        document.querySelector('#RoleChar').innerHTML = char;
+    
+
+    function handleChange(event) {
+        setTask(event.target.value);
+        
     }
 
-    else if(roleVar === 'admin'){
-        const char = `
-            <div> You are Admin </div>
-        `;
-        document.querySelector('#RoleChar').innerHTML = char;
+    function handleAdd() {
+        const newTask = tasks.concat({task});
+
+        setTasks(newTask);
+        setTask('');
     }
 
-    return(
+
+    return (
         <>
             <h1>To-do</h1>
-            <div style={{fontSize:'40px'}} id="Role"></div>
-            <div style={{fontSize:'40px'}} id="RoleChar"></div>
+
+
+            <div>
+                <input id='input' type="text" value={task} onChange={handleChange} />
+                <button id='button' type="button" onClick={handleAdd}>
+                    Add
+                </button>
+            </div>
+
+            <ul>
+                {tasks.map((item) => (
+                    <li key={item.prior}>{item.task}</li>
+                ))}
+            </ul>
+
+
+
+
             <button onClick={() => signOut(auth).then(() => {
                 window.location.href = '/login';
             })
-            .catch((error) => {
-                alert("Error")
-            })}>Sign Out</button>
+                .catch((error) => {
+                    alert("Error")
+                })}>Sign Out</button>
         </>
     )
 }
